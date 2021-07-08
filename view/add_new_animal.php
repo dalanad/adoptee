e8eb80fda3809ec50d72c7a90400f09647df464e
Ovini <ovinim@gmail.com>
2021-07-09 02:37:35 +0530

    add_new_animal(2)

--- view/add_new_animal.php
+++ view/add_new_animal.php
@@ -4,6 +4,21 @@ require dirname(__FILE__) . "./_layout/header.php"
 // include "conn.php";
 ?>
 
+<style>
+.ctrl2 {
+	padding: 0.4em 0.5em;
+	line-height: 1;
+	border-radius: 8px;
+	font-family: inherit;
+	color: var(--color);
+	font-size: 1rem;
+	border: 0.2rem solid var(--gray-2);
+	background: var(--gray-1);
+	width: 20%;
+	box-sizing: border-box;
+}
+</style>
+
 <div class='container px2'>
     <div class='placeholder-box mr1' style='height:50px; width:100px;'>Logo</div>
     <h2 class='mt1 txt-clr'>Add New Animal for Adoption</h2>
@@ -15,16 +30,21 @@ require dirname(__FILE__) . "./_layout/header.php"
 
         <div class='field'>
             <label for='gender'>Gender</label>
-            <input class="ctrl" type="text" name="gender" placeholder="Male / Female" required/>
+                <select class="ctrl" name='gender' required>
+                    <option selected='true' disabled='disabled'>Select</option>
+                    <option value='male'>Male</option>
+                    <option value='female'>Female</option>
+                </select>
         </div>
 
         <div class='field'>
             <label for='age'>Age</label>
             <div>
-            <label for='years'>Years</label>
-            <input class="ctrl" type="number" step="1" min="0" name="years" placeholder="Years" required/>
-            <label for='months'>Months</label>
-            <input class="ctrl" type="number" step="1" min="0" max="11" name="months" placeholder="Months" required/>
+            
+            <input class="ctrl2" type="number" step="1" min="0" name="years" placeholder="Years" required/>
+            <input class="ctrl2" type="number" step="1" min="0" max="11" name="months" placeholder="Months" required/>
+            <input class="ctrl2" type="number" step="1" min="0" max="3" name="months" placeholder="Weeks" required/>
+            <input class="ctrl2" type="number" step="1" min="0" max="6" name="months" placeholder="Days" required/>
             </div>
         </div>
 
@@ -42,8 +62,9 @@ require dirname(__FILE__) . "./_layout/header.php"
             </div>
             <span class="field-msg"> </span>
         </div>
+        <br>
 
-        <button class='btn mr2' type='reset'>Clear All</button>
+        <button class='btn mr2' type='reset'>Clear</button>
         <button class='btn mr2'>Add</button>
     </form>
 
