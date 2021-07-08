d43f209c6a30dcafb8fbb5bae476b7b05cf79f07
Dalana Pasindu <dalana199981@gmail.com>
2021-07-08 21:31:14 +0530

    fixed formating on donations page

--- view/donations.php
+++ view/donations.php
@@ -1,78 +1,35 @@
 <?php
-require_once  dirname(__FILE__) .'./_layout/layout.php';
+require_once  dirname(__FILE__) . './_layout/layout.php';
 require dirname(__FILE__) . "./_layout/header.php"
-// include "conn.php"
+// include "conn.php";
 ?>
 
-<div class = 'container'>
-    <div class = 'placeholder-box mr1' style = 'height:50px; width:100px;'>Logo</div>
-    <h2 class = 'mt1 txt-clr'>Donate to Tails of Freedom</h2> <!--fetch name of org and display-->
+<div class='container px2'>
+    <div class='placeholder-box mr1' style='height:50px; width:100px;'>Logo</div>
+    <h2 class='mt1 txt-clr'>Donate to Tails of Freedom</h2>
+    <!--fetch name of org and display-->
     <form>
-        <!-- <div class = 'field'>
-
-            <select name = 'orgs' required>-->
-
-                <!-- fetch org names from db and display in dropdown -->
-                <?php
-                    /*$orgs = mysqli_query($db, "SELECT name FROM Organization")
-                    while($data = mysqli_fetch_assoc($orgs)){
-                    echo "<option value = '" . $data['name'] . "'>" . $data['name'] . "</option>"
-                    }*/
-                ?>
-                <!-- <option selected = 'true' disabled = 'disabled'>Select Organization</option>
-                <option value = 'tails of freedom'>Tails of Freedom</option>
-                <option value = 'cat protection trust'>Cat Protection Trust</option>
-            </select>
-        </div> -->
-
-        <div class = 'field'>
-
-            <select name = 'amount' required>
-
-                <?php
-                    /*$amt = mysqli_query($db, "SELECT amount FROM Organization WHERE name = 'org'")
-                    while($data = mysqli_fetch_assoc($amt)){
-                    echo "<option value = '" . $data['amt'] . "'>" . $data['amt'] . "</option>"
-                }*/
-                ?>
-
-                <option selected = 'true' disabled = 'disabled'>Amount</option>
-                <option value = '5000'>Rs. 5000</option>
-                <option value = '10000'>Rs. 10000</option>
-                <option value = '15000'>Rs. 15000</option>
+        <div class='field'>
+            <select class="ctrl" name='amount' required>
+                <option selected='true' disabled='disabled'>Amount</option>
+                <option value='5000'>Rs. 5000</option>
+                <option value='10000'>Rs. 10000</option>
+                <option value='15000'>Rs. 15000</option>
             </select>
         </div>
 
-        <div class = 'field'>
-
-            <label for = 'name'>Name to be displayed (optional)</label>
-
-            <?php
-                /*select user's email as $data
-                echo "<input type = "text" placeholder = '" .$data['email'] ."'/>"*/
-            ?>
-
-            <input type = "text" name = "name" placeholder = "Hiruni"/>
-
+        <div class='field'>
+            <label for='name'>Name to be displayed (optional)</label>
+            <input class="ctrl" type="text" name="name" placeholder="Hiruni" />
         </div>
 
-        <div class = 'field'>
-
-            <label for = 'email'>Email</label>
-
-            <?php
-                /*select user's email as $data
-                echo "<input type = "text" value = '" .$data['email'] ."'/>"*/
-            ?>
-
-            <input type = "text" name = "email" value = "hiruni@gmail.com" required/>
-
+        <div class='field'>
+            <label for='email'>Email</label>
+            <input class="ctrl" readonly name="email" value="hiruni@gmail.com" required />
         </div>
-
         <p>If you subscribe, the first donation will be made immediately, and monthly thereafter</p>
-        <a class = 'btn' href = '' style = "margin-right:20px;" type = 'reset'>Clear all</a>
-        <a class = 'btn' href = '' style = "margin-right:20px;">Donate</a>
-        <a class = 'btn' href = ''>Subscribe</a>
-
+        <button class='btn mr2' type='reset'>Clear all</button>
+        <button class='btn mr2'>Donate</button>
     </form>
+
 </div>
