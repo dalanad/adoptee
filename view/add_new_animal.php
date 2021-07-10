f9e02e26bf3aefc945895f88a85dcc237ae210b9
Ovini <ovinim@gmail.com>
2021-07-10 22:47:35 +0530

    added jquery link

--- view/add_new_animal.php
+++ view/add_new_animal.php
@@ -27,7 +27,6 @@ require dirname(__FILE__) . "./_layout/header.php"
 
 </style>
 
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script>
     $(document).ready(function() {
       $("select").change(function() {
