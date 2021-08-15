7715cd05926cb04ff045e7a438e155a3707a3814
Dalana Pasindu <dalana199981@gmail.com>
2021-08-15 08:28:30 +0530

    Simplify Organization Registration

--- view/auth/sign_up_doctor.php
+++ view/auth/sign_up_doctor.php
@@ -1,33 +1,6 @@
 <?php require_once __DIR__ . "./../_layout/layout.php" ?>
 <link rel="stylesheet" href="/assets/css/auth.css" type="text/css">
 
-<style>
-    .separator {
-        margin: 1rem 0;
-        grid-column: 1 / -1;
-        display: flex;
-        align-items: center;
-        color: var(--gray-5);
-        font-weight: 300;
-        line-height: 1rem;
-    }
-
-    .form-grid {
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-        grid-column-gap: .5rem;
-    }
-
-    .separator::after {
-        height: 1px;
-        content: " ";
-        display: block;
-        background: var(--gray-2);
-        margin-left: .5rem;
-        flex: 1 1 0;
-    }
-</style>
-
 <div class="centered-container">
     <div class="card-container" style="max-width: 500px; width: 100%;">
         <form style="margin: 1rem; flex: 1 1 0; max-width: 500px;" action="/doctor/process_registration" method="POST">
@@ -37,7 +10,6 @@
                     <div> Welcome to Adoptee </div>
                     <div style="font-size:1.5rem">Register As A Doctor </div>
                 </div>
-
             </div>
             <div class="form-grid">
                 <div class="separator body-text"><i class="far fa-user"></i> &nbsp; User Information</div>
---
+++ view/auth/user_verification.php
@@ -0,0 +1,91 @@
+<?php require_once __DIR__ . "./../_layout/layout.php" ?>
+<?php $step = isset($_GET["step"]) ? $_GET["step"] : 1; ?>
+
+<link rel="stylesheet" href="/assets/css/auth.css" type="text/css">
+<style>
+    .heading {
+        padding: 1rem;
+        display: flex;
+        align-items: flex-end;
+        justify-content: space-between;
+    }
+
+    .heading img {
+        height: 100px;
+    }
+
+    .animated-card {
+        width: 100%;
+    }
+
+    .lg-3col-grid {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        column-gap: 1rem;
+    }
+
+    @media (max-width:600px) {
+        .heading img {
+            height: 80px;
+        }
+
+        .lg-3col-grid {
+            grid-template-columns: 1fr;
+        }
+
+    }
+</style>
+<div class="centered-container">
+    <div>
+        <div style="margin: 0 auto;margin-bottom: 5rem;max-width: 400px;">
+            <div class="heading">
+                <div>
+                    <div>Welcome to Adoptee</div>
+                    <h2 class="m0">User Verification</h2>
+                </div>
+                <img src="/assets/images/auth/security.webp">
+            </div>
+
+            <div style="display: flex;">
+                <div class="animated-card " id="step-3">
+                    <style>
+                        .verify-card {
+                            border: .1rem solid var(--gray-3);
+                            width: 100%;
+                            padding: .5rem .8rem;
+                            margin: .2rem 0;
+                            border-radius: .4rem;
+
+                            line-height: 1;
+                        }
+
+                        .verify-card-heading {
+                            display: flex;
+                            justify-content: space-between;
+                        }
+                    </style>
+                    <div style="display: flex;flex-direction:column;align-items:center;margin:1rem">
+                        <div class="verify-card">
+                            <div class="verify-card-heading"> Email <i class="txt-clr green fa  fa-check-circle"></i></div>
+                        </div>
+                        <div class="verify-card">
+                            <div class="verify-card-heading">Telephone
+                                <i class="txt-clr orange fas fa-exclamation-circle"></i>
+                            </div>
+                            <div>
+                                <div style="font-weight: 600;text-align:center;margin:.5rem 0">OTP</div>
+                                <input type="text" inputmode="numeric" required pattern="\d{6}" autocomplete="one-time-code" class="ctrl">
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex justify-between mt2">
+                        <!-- <a class="btn outline pink" href="?step=2">Back</a> -->
+                        <a class="btn" href="/view/org/dashboard.php">Continue</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
