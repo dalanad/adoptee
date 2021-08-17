33916084a8860192d990b933ce75d1897f7c849c
Ovini <ovinim@gmail.com>
2021-08-17 19:20:08 +0530

    AddNewAnimal Controller created, database updated

---
+++ controllers/AddNewAnimalController.php
@@ -0,0 +1,15 @@
+<?php
+
+class AddNewAnimalController extends Controller{
+
+    function view()
+    {
+        View::render("view/org/org_dashboard/add_new_animal");
+    }
+
+    function submit(){
+        AddNewAnimal::createNewAnimal($_POST['org_id'], $_POST['animal_id'], $_POST['animal_type'], $_POST['gender'], $_POST['age_years'], $_POST['age_months'], $_POST['age_weeks'], $_POST['age_days'], $_POST['color'], $_POST['animal_description'], $_POST['photo']);
+    }   
+}
+
+?>
