4aec6b931acee8c8491bb813993b83d4dd090142
Hiruni <hiruni.nd@gmail.com>
2021-08-19 17:09:17 +0530

    update email verification in user table

--- models/User.php
+++ models/User.php
@@ -11,15 +11,24 @@ class User extends BaseModel
     static function updateProfileData($name, $email, $telephone, $address)
     {
         $query = "UPDATE `user` SET 
-        name = '$name' WHERE isset($name),
-        email = '$email' WHERE isset($email),
-        telephone = $telephone WHERE isset($telephone),
-        address = '$address' WHERE isset($address)";
+        name = '$name' ,
+        -- WHERE isset($name),
+        email = '$email',
+        --  WHERE isset($email),
+        telephone = $telephone,
+        --  WHERE isset($telephone),
+        address = '$address',
+        --  WHERE isset($address)";
         return BaseModel::insert($query);
     }
 
     static function findUserByEmail($email){
         $query = "select *  from `user` where email= '$email'";
         return BaseModel::select($query)[0];
+    }
+
+    static function verifyEmail($email){
+        $query = "update `user` set email_verified= 1 where email= $email";
+        return BaseModel::update($query);
     }
 }
