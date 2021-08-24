ac5055fe7242185c07de623a847c29241897b79e
Hiruni <hiruni.nd@gmail.com>
2021-08-25 00:55:07 +0530

    reset password page, database errors

--- view/auth/password_reset/request_link.php
+++ view/auth/password_reset/request_link.php
@@ -3,36 +3,38 @@
 <link rel="stylesheet" href="/assets/css/auth.css" type="text/css">
 
 <div class="centered-container">
-<div class="card-container">
-<div class="animated-card" id="sign-up">
-<div>
+    <div class="card-container">
+        <div class="animated-card" id="sign-up">
+            <div>
 
-    <div class = "m2" id = "image" style="text-align:center;">
-        <img style="border-radius:8px; height:50px;" 
-        src="/assets/images/logo.png" />
-    </div>
+                <div class="m2" id="image" style="text-align:center;">
+                    <img style="border-radius:8px; height:50px;" src="/assets/images/logo.png" />
+                </div>
 
-    <div class = "title-text center">
-    <label style = "font-size:1.1rem;">Reset Password</label>
-    </div>
+                <div class="title-text center">
+                    <label style="font-size:1.1rem;">Reset Password</label>
+                </div>
 
-    <div class = "body-text relative">
-        Please enter your email below to reset the password
-    </div>
-    <!-- <p class = "center wrap"> </p>-->
-    
-    <div class = "field">
-        <label>Email</label>
-        <input class = "ctrl" type = "email">
-        <span class = "field-msg"></span>
-    </div>
+                <div class="body-text relative">
+                    Please enter your email below to reset the password
+                </div>
+                <!-- <p class = "center wrap"> </p>-->
 
-    <div class = "field center mt1"><button class = "btn orange">Reset Pasword</btn></div>
+                <form action="/Auth/request_link" method="POST">
+                    <div class="field">
+                        <label>Email</label>
+                        <input class="ctrl" type="email">
+                        <span class="field-msg"></span>
+                    </div>
+                </form>
 
-    <div class = "center mt2" style = "font-size:0.8rem;">
-        <a class = "btn-link bold text-decoration-none" href = "./../sign_in.php">Back to Login</a>
+                <div class="field center mt1"><button class="btn orange" type="submit">Reset Pasword</btn>
+                </div>
+
+                <div class="center mt2" style="font-size:0.8rem;">
+                    <a class="btn-link bold text-decoration-none" href="./../sign_in.php">Back to Login</a>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-</div>
-</div>
 </div>
