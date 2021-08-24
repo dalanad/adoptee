77cd74d16331f223faa92023aa4d05e5335af2bf
Ovini <ovinim@gmail.com>
2021-08-24 21:31:06 +0530

    minor changes done

--- controllers/AdoptionAnimalsController.php
+++ controllers/AdoptionAnimalsController.php
@@ -8,16 +8,21 @@ class AdoptionAnimalsController extends Controller{
     }
 
     function process_add_new_animal(){
-        AdoptionAnimal::createNewAnimal($_POST['org_id'], $_POST['animal_id'], $_POST['animal_type'], $_POST['gender'], $_POST['age_years'], $_POST['age_months'], $_POST['age_weeks'], $_POST['age_days'], $_POST['color'], $_POST['animal_description'], $_POST['photo']);
+        AdoptionAnimals::createNewAnimal(1, 1, $_POST['type'], $_POST['other'], $_POST['gender'], $_POST['dob'], $_POST['color'], $_POST['description'], $_POST['photo']);
     }  
     
     function animals_for_adoption()
     {
         $data = [
             "active" => "org_adoption_listing",
-            "animals"=>AdoptionAnimals::findAnimalsByOrgId('1')
+            "animals"=>AdoptionAnimals::findAnimalsByOrgId(1)
     ];
         View::render("org/dashboard", $data);
+    }
+
+    function reported_cases()
+    {
+        View::render("org/org_dashboard/reported_cases");
     }
 }
 
