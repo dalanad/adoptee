8cdb03a735044bfd76d8d7290948642c10c5bf43
Dalana Pasindu <dalana199981@gmail.com>
2021-09-13 11:18:23 +0530

    fix : require paths

--- view/doctor/_nav.php
+++ view/doctor/_nav.php
@@ -1,4 +1,4 @@
-<?php require_once __DIR__ . './../_layout/layout.php' ?>
+<?php require_once __DIR__ . '/../_layout/layout.php' ?>
 <link rel="stylesheet" href="/assets/css/doctor.css">
 <?php
 $menu_items = array(
