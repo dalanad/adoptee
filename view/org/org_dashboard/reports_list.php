9e12d073632a93fc28fb84114922a532b217c130
Dalana Dharmathilake <dalana199981@gmail.com>
2022-03-27 23:59:15 +0530

    added image icons for org reports

--- view/org/org_dashboard/reports_list.php
+++ view/org/org_dashboard/reports_list.php
@@ -1,8 +1,57 @@
-<h3 style="text-align: center;">Select Report Type</h3>
-<div style="justify-content:center; align-items:center; display:flex;flex-wrap:wrap">
-    <a href="/OrgManagement/animals_report" class="btn green" style="display:inline-flex;margin:1rem 0; width: 80px">Animal Report</a>&emsp;&emsp;
-    <a href="/OrgManagement/adoptions_updates" style="display:inline-flex;margin:1rem 0; width: 80px" class="btn blue">Adoptions Updates</a> &emsp;&emsp;
-    <a href="/OrgManagement/rescue_to_adoption" style="display:inline-flex;margin:1rem 0; width: 80px" class="btn orange">Rescue To Adoption</a> &emsp;&emsp;
-    <a href="/OrgManagement/rescues_information" style="display:inline-flex;margin:1rem 0; width: 80px" class="btn pink">Rescues Information</a> &emsp;&emsp;
-    <a href="/OrgManagement/donations_summary" style="display:inline-flex;margin:1rem 0; width: 80px" class="btn">Donations Summary</a> &emsp;&emsp;
+<h2 style="text-align: center;font-weight:400">Available Reports</h3>
+<div class="grid">
+    <a href="/OrgManagement/animals_report" class="report-item">
+        <img src="/assets/images/org/reports/animals.svg" class="report-img">
+        Animal Details</a>&emsp;&emsp;
+    <a href="/OrgManagement/adoptions_updates" class="report-item">
+        <img src="/assets/images/org/reports/adoption_updates.svg" class="report-img">
+        Adoptions Updates</a> &emsp;&emsp;
+    <a href="/OrgManagement/rescue_to_adoption" class="report-item">
+        <img src="/assets/images/org/reports/rescue_time.svg" class="report-img">
+        Rescue To Adoption</a> &emsp;&emsp;
+    <a href="/OrgManagement/rescues_information" class="report-item">
+        <img src="/assets/images/org/reports/ambulance.png" class="report-img">
+        Rescues Information</a> &emsp;&emsp;
+    <!-- <a href="/OrgManagement/donations_summary" class="report-item">
+        <img src="/assets/images/org/reports/donate.png" class="report-img">
+        Donations Summary</a> &emsp;&emsp; -->
 </div>
+
+<style>
+    .grid {
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        flex-wrap: wrap
+    }
+
+    .report-item {
+        display: block;
+        margin: 1rem 0;
+        width: 180px;
+        padding: 1rem;
+        height: 150px;
+        background: white;
+        box-shadow: var(--shadow-light);
+        border-radius: 1rem;
+        text-decoration: none;
+        color: black;
+        font-weight: 400;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid var(--muted);
+    }
+
+    .report-img {
+        height: 100px;
+        margin-bottom: 1.5rem;
+    }
+
+    .report-item:hover {
+        cursor: pointer;
+        background: #fafafa;
+    }
+</style>
